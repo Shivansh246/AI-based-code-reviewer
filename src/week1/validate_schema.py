b55175eec5963797ee65a53bff329c7c4e60d62a
Shivansh246 <285635402+Shivansh246@users.noreply.github.com>
2026-09-06 22:45:19 +0530

    Define and validate vulnerability result schema

---
+++ src/week1/validate_schema.py
@@ -0,0 +1,12 @@
+import json
+from jsonschema import validate
+
+with open("schemas/vulnerability.json") as file:
+    schema = json.load(file)
+
+with open("schemas/example_static.json") as file:
+    finding = json.load(file)
+
+validate(instance=finding, schema=schema)
+
+print("Finding is valid!")
